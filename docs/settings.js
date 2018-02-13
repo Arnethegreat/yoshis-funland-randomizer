90863f55c258d8b8b904caa883ed9247db92050e
Patrick Nance <jpnance@gmail.com>
2018-02-12 20:30:33 -0800

    Big monolithic commit that restructures and refactors several things. New functionality is that forts and castles are fixed to x-4 and x-8 tiles and 6-8 will always appear on 6-8.

--- docs/settings.js
+++ docs/settings.js
@@ -26,57 +26,10 @@ let modes = [
     powerfulYoshi, poisonCoins
     ];
 
-var normalLevels;
-var fortressLevels;
-var castleLevels;
-var autoscrollers;
-
-let getSettings = function() {
-    boost = document.getElementById("boost").checked;
-    filledMouth = document.getElementById("filledMouth").checked;
-    stickyGround = document.getElementById("stickyGround").checked;
-    extendedFlutter = document.getElementById("extendedFlutter").checked;
-    deathStarCounter = document.getElementById("deathStarCounter").checked;
-    hardMode = document.getElementById("hardMode").checked;
-    drunkMode = document.getElementById("drunkMode").checked;
-    noTongue = document.getElementById("noTongue").checked;
-    noFlutter = document.getElementById("noFlutter").checked;
-    bouncyCastle = document.getElementById("bouncyCastle").checked;
-    randomCursor = document.getElementById("randomCursor").checked;
-    reverseControl = document.getElementById("reverseControl").checked;
-    turbo = document.getElementById("turbo").checked;
-    powerfulYoshi = document.getElementById("powerfulYoshi").checked;
-    poisonCoins = document.getElementById("poisonCoins").checked;
-    extraLevels = document.getElementById("extraLevels").checked;
-    bonusLevels = document.getElementById("bonusLevels").checked;
-    randomLevelOrder = document.getElementById("randomLevelOrder").checked;
-
-    modes = [
-    boost, filledMouth, stickyGround,
-    extendedFlutter, deathStarCounter, hardMode,
-    drunkMode, noTongue, noFlutter, bouncyCastle,
-    randomCursor, reverseControl, turbo,
-    powerfulYoshi, poisonCoins
-    ];
-
-    normalLevels = []
-    fortressLevels = []
-    castleLevels = []
-    autoscrollers = []
-}
-
-let maxModesInputVal = parseInt(document.getElementById("maxEnabledModes").value);
-let maxEnabledModes = maxModesInputVal !== NaN ? maxModesInputVal : 100;
+//let maxModesInputVal = parseInt(document.getElementById("maxEnabledModes").value);
+//let maxEnabledModes = maxModesInputVal !== NaN ? maxModesInputVal : 100;
 
 let levelModeFilter = {};
-
-let levelIndexes = [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,
-                    0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15,
-                    0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, 0x20, 0x21,
-                    0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D,
-                    0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39,
-                    0x3C, 0x3D, 0x3E, 0x3F, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45
-];
 
 /**
  * Shuffles array in place.
@@ -102,32 +55,60 @@ function shuffle(array) {
     return array;
 }
 
-let generateLevelOrder = function() {
-    getSettings()
-    var customLevelOrder = []
+function randomizeLevelOrder(rom, options) {
+    var levelIndexes = {
+        bowser: 0x43,
+        castles: [ 0x07, 0x13, 0x1F, 0x2B, 0x37 ], // not including King Bowser's Castle because it's a special case
+        forts: [ 0x03, 0x0F, 0x1B, 0x27, 0x33, 0x3F ],
+        normals: [
+            0x00, 0x01, 0x02, 0x04, 0x05, 0x06,
+            0x0C, 0x0D, 0x0E, 0x10, 0x11, 0x12,
+            0x18, 0x19, 0x1A, 0x1C, 0x1D, 0x1E,
+            0x24, 0x25, 0x26, 0x28, 0x29, 0x2A,
+            0x30, 0x31, 0x32, 0x34, 0x35, 0x36,
+            0x3C, 0x3D, 0x3E, 0x40, 0x41, 0x42
+        ],
+        extras: [ 0x08, 0x14, 0x20, 0x2C, 0x38, 0x44 ],
+        bonuses: [ 0x09, 0x15, 0x21, 0x2D, 0x39, 0x45 ]
+    };
 
-    for (let index = 0; index < 6; index++) {
-        var worldNumber = index*10
-        var normalLevels = levelIndexes.slice(worldNumber, worldNumber + 8)
-        var extraLevel = levelIndexes.slice(worldNumber + 8, worldNumber + 9)
-        var bonusLevel = levelIndexes.slice(worldNumber + 9, worldNumber + 10)
-        customLevelOrder = customLevelOrder.concat(normalLevels)
+    var customLevelOrder = [];
 
-        if (extraLevels) {
-            customLevelOrder = customLevelOrder.concat(extraLevel)
-        }
-
-        if (bonusLevels) {
-            customLevelOrder = customLevelOrder.concat(bonusLevel)
-        }
+    if (options.includeExtras) {
+        // we'll get here later
     }
 
-    if (randomLevelOrder) {
-        customLevelOrder = shuffle(customLevelOrder)
+    levelIndexes.castles = shuffle(levelIndexes.castles);
+    levelIndexes.forts = shuffle(levelIndexes.forts);
+    levelIndexes.normals = shuffle(levelIndexes.normals);
+
+    for (var world = 1; world <= 6; world++) {
+        customLevelOrder.push(levelIndexes.normals.shift()); // x-1
+        customLevelOrder.push(levelIndexes.normals.shift()); // x-2
+        customLevelOrder.push(levelIndexes.normals.shift()); // x-3
+
+        customLevelOrder.push(levelIndexes.forts.shift()); // x-4, specifically use a fort
+
+        customLevelOrder.push(levelIndexes.normals.shift()); // x-5
+        customLevelOrder.push(levelIndexes.normals.shift()); // x-6
+        customLevelOrder.push(levelIndexes.normals.shift()); // x-7
+
+        if (world == 6) {
+            customLevelOrder.push(levelIndexes.bowser); // 6-8, always put 6-8 at the end
+        }
+        else {
+            customLevelOrder.push(levelIndexes.castles.shift()); // x-8, specifically use a castle
+        }
+
+        customLevelOrder.push(levelIndexes.extras.shift()); // x-E
+        customLevelOrder.push(levelIndexes.bonuses.shift()); // x-B
     }
 
     console.log(customLevelOrder)
-    return customLevelOrder
+
+    customLevelOrder.forEach(function(level, i) {
+        rom[LEVEL_SETTINGS + LEVEL_OFFSETS[i]] = level;
+    });
 }
 
 let randomBinary = function() {
@@ -178,18 +159,46 @@ function saveAs(blob, fileName) {
     }, 1000);
 }
 
+function getOptions() {
+    return {
+        randomLevelOrder: document.getElementById('randomLevelOrder').checked
+
+        /* to be enabled as we get to them */
+
+        // boost: document.getElementById('boost').checked,
+        // filledMouth: document.getElementById('filledMouth').checked,
+        // stickyGround: document.getElementById('stickyGround').checked,
+        // extendedFlutter: document.getElementById('extendedFlutter').checked,
+        // deathStarCounter: document.getElementById('deathStarCounter').checked,
+        // hardMode: document.getElementById('hardMode').checked,
+        // drunkMode: document.getElementById('drunkMode').checked,
+        // noTongue: document.getElementById('noTongue').checked,
+        // noFlutter: document.getElementById('noFlutter').checked,
+        // bouncyCastle: document.getElementById('bouncyCastle').checked,
+        // randomCursor: document.getElementById('randomCursor').checked,
+        // reverseControl: document.getElementById('reverseControl').checked,
+        // turbo: document.getElementById('turbo').checked,
+        // powerfulYoshi: document.getElementById('powerfulYoshi').checked,
+        // poisonCoins: document.getElementById('poisonCoins').checked,
+        // extraLevels: document.getElementById('extraLevels').checked,
+        // bonusLevels: document.getElementById('bonusLevels').checked
+    };
+}
+
 var LEVEL_SETTINGS = 0x118026;
 var LEVEL_OFFSETS = [
-//   1   2   3   4   5   6   7   8  //  E   B
-     1,  2,  3,  4,  5,  6,  7,  8, //  9, 10, // World 1
-    13, 14, 15, 16, 17, 18, 19, 20, // 21, 22, // World 2
-    25, 26, 27, 28, 29, 30, 31, 32, // 33, 34, // World 3
-    37, 38, 39, 40, 41, 42, 43, 44, // 45, 46, // World 4
-    49, 50, 51, 52, 53, 54, 55, 56, // 57, 58, // World 5
-    61, 62, 63, 64, 65, 66, 67, 68, // 69, 70  // World 6
+//   1   2   3   4   5   6   7   8   E   B
+     1,  2,  3,  4,  5,  6,  7,  8,  9, 10, // World 1
+    13, 14, 15, 16, 17, 18, 19, 20, 21, 22, // World 2
+    25, 26, 27, 28, 29, 30, 31, 32, 33, 34, // World 3
+    37, 38, 39, 40, 41, 42, 43, 44, 45, 46, // World 4
+    49, 50, 51, 52, 53, 54, 55, 56, 57, 58, // World 5
+    61, 62, 63, 64, 65, 66, 67, 68, 69, 70  // World 6
 ];
 
 function generateRom() {
+    var options = getOptions();
+
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'build.sfc', true);
     xhr.responseType = 'arraybuffer';
@@ -198,11 +207,13 @@ function generateRom() {
         if (this.status == 200) {
             var buffer = xhr.response;
             var rom = new Uint8Array(buffer);
-            var levelOrder = generateLevelOrder();
 
-            levelOrder.forEach(function(level, i) {
-                rom[LEVEL_SETTINGS + LEVEL_OFFSETS[i]] = level;
-            });
+            if (options.randomLevelOrder) {
+                randomizeLevelOrder(rom, options);
+            }
+
+            // this is the L+R on file 3 debug flag
+            rom.set([ 0xEA, 0xEA, 0xEA ], 0xB9897);
 
             saveAs(new Blob([buffer], { type: 'application/octet-stream' }), 'yoshfun.sfc');
         }
