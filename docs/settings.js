9716c790b6955a343d57a13a5c9048ed00f9afc3
Tim Alakangas <Arnethegreat@gmail.com>
2018-02-14 08:37:58 +0100

    update pointers

--- docs/settings.js
+++ docs/settings.js
@@ -185,7 +185,7 @@ function getOptions() {
     };
 }
 
-var WORLD_MAPS = 0x118026;
+var WORLD_MAPS = 0x118028;
 var LEVEL_OFFSETS = [
 //   1   2   3   4   5   6   7   8   E   B
      1,  2,  3,  4,  5,  6,  7,  8,  9, 10, // World 1
@@ -195,7 +195,7 @@ var LEVEL_OFFSETS = [
     49, 50, 51, 52, 53, 54, 55, 56, 57, 58, // World 5
     61, 62, 63, 64, 65, 66, 67, 68, 69, 70  // World 6
 ];
-var LEVEL_SETTINGS = 0x11806F;
+var LEVEL_SETTINGS = 0x118071;
 var LEVEL_SETTINGS_ENDMARKER = [ 0x89, 0x80 ]; // this is reversed because of its endianness
 
 function generateRom() {
